79b81728ddd967df297925cd4be6c35dfe7bd0c3
Andre Smith <andrepsmithjr@gmail.com>
2014-03-01 19:10:28 -0800

    Start writing tests

--- test/list_test.rb
+++ test/list_test.rb
@@ -1,2 +1,22 @@
 require 'test_helper'
 require 'list'
+
+
+describe "Traversing a singly linked list" do
+  it "should return '1,2,3,4,5,6,7,8' for a list made up of the numbers 1-8"do
+    list = create_sll
+
+    list.traverse
+
+  end
+end
+
+describe "Traversing a doubly linked list" do
+  it "should return '1,2,3,4,5,6,7,8,1' for a doubly linked list made up of numbers 1-8" do
+  end
+end
+
+describe "Deleting a node from a linked list" do
+  it "should return 1,2,3,5,6,7,8 if the 4th node is deleted from a linked list of 1-8" do
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,21 @@
 require 'minitest/autorun'
 require 'minitest/spec'
+
+def create_sll
+  head = Node.new(1)
+  node2 = Node.new(2,head)
+  node3 = Node.new(3,node2)
+  node4 = Node.new(4,node3)
+  node5 = Node.new(5,node4)
+  node6 = Node.new(6,node5)
+  node7 = Node.new(7,node6)
+  node8 = Node.new(8,node7)
+
+  node7.nexxt = node8
+  node6.nexxt = node7
+  node5.nexxt = node6
+  node4.nexxt = node5
+  node3.nexxt = node4
+  node2.nexxt = node3
+  head.nexxt = node2
+end
